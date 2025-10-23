41f19e810ffd251d0221ee9da4f25ef3f5066868
Fats77 <fatimazebtanoli6@gmail.com>
2025-10-23 11:07:13 +0800

    Initial Commit

---
+++ Heimdall/HeimdallApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeimdallApp.swift
+//  Heimdall
+//
+//  Created by Fatima Zeb on 23/10/25.
+//
+
+import SwiftUI
+
+@main
+struct HeimdallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
