f1b5374dcacb36ea1c52c08eb9f36f8e358f68a1
Fats77 <fatimazebtanoli6@gmail.com>
2025-10-29 11:12:47 +0800

    feat: Attendance screen design

---
+++ Heimdall/Views/CoordinatorViews/AttendanceView.swift
@@ -0,0 +1,18 @@
+//
+//  AttendanceView.swift
+//  Heimdall
+//
+//  Created by Fatima Zeb on 28/10/25.
+//
+
+import SwiftUI
+
+struct AttendanceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AttendanceView()
+}
