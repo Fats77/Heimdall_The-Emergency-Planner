fb3aa07391c524d7274ebe63fdfca0d7411df7bc
Fats77 <fatimazebtanoli6@gmail.com>
2025-10-30 16:54:56 +0800

    feat: added, create plan screen

---
+++ Heimdall/Views/AdminViews/CreatePlan.swift
@@ -0,0 +1,18 @@
+//
+//  CreatePlan.swift
+//  Heimdall
+//
+//  Created by Fatima Zeb on 30/10/25.
+//
+
+import SwiftUI
+
+struct CreatePlan: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreatePlan()
+}
