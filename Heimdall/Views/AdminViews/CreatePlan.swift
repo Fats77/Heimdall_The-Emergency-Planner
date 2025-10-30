77d9303a3bd0d6065806d7e477002000951a5974
Fats77 <fatimazebtanoli6@gmail.com>
2025-10-30 16:55:15 +0800

    feat: created plan view

--- Heimdall/Views/AdminViews/CreatePlan.swift
+++ Heimdall/Views/AdminViews/CreatePlan.swift
@@ -9,7 +9,15 @@ import SwiftUI
 
 struct CreatePlan: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView{
+            Text ("Select Icon")
+                
+            HStack{
+                
+            }
+            
+            
+        }
     }
 }
 
