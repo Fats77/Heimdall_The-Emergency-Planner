7d916bf9614fb550b536d0d0932b319e98d4f717
Fats77 <fatimazebtanoli6@gmail.com>
2025-10-30 11:53:21 +0800

    feat: onboarding screen added

--- Heimdall/ContentView.swift
+++ Heimdall/ContentView.swift
@@ -9,14 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            
+            Tab ( "Overview", systemImage: "bolt")
+            {HomeView()}
+            Tab ("Sccan" , systemImage: "qrcode.viewfinder")
+            {
+                AttendanceView()
+            }
+           
+            
+            
         }
-        .padding()
     }
+    
 }
 
 #Preview {
--- Heimdall/Views/OnboardingView.swift
+++ Heimdall/Views/OnboardingView.swift
@@ -8,8 +8,39 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    @State private var currentPage = 0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+       
+        VStack(alignment: .center, spacing: 10){
+            
+            Image(.helmet)
+                .resizable()
+                .scaledToFit()
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .frame(width: 150, height: 150)
+                .padding(50)
+                .shadow(color: .tertiary.opacity(0.7), radius:20, y: 10)
+
+        Text("Welcome to Heimdall")
+           // .font(.headline)
+            .font(.title)
+            .foregroundStyle(Color.secondary2)
+            .fontWeight(.bold)
+        Text("The Emergency Planner")
+                .foregroundStyle(.primary2)
+            .font(.title2)
+            .bold()
+    
+        Text("Your comprehensive disaster preparedness and emergency response platform")
+                .multilineTextAlignment(.center)
+                .foregroundStyle(Color.tertiary)
+                .font(.title3)
+                .padding(.top,50)
+        }
+        .padding(40)
+        
+        
     }
 }
 
